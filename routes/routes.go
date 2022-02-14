143f3cbaa31075a7f6a9bb718bce2e925d9edd0f
Anuj Koli <a.koli@ufl.edu>
2022-02-14 00:15:19 -0500

    Added middleware to router

--- routes/routes.go
+++ routes/routes.go
@@ -1,28 +1,41 @@
-
 package routes
 
 import (
 	"gatorshare/controllers"
+	"gorm.io/gorm"
+	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
-func InitializeRoutes(router *gin.Engine) {
+func InitializeRoutes(db *gorm.DB) *gin.Engine {
+
+	router := gin.Default()
+
+	router.Use(gin.Logger())
+
+	router.GET("/", func(responseCtx *gin.Context) {
+	  responseCtx.JSON(http.StatusOK, gin.H{"data": "Welcome to Gatorshare made with the help of Go and Gin!"})    
+	})
+
+	api := controllers.Controller{DB: db}
 
 	v1 := router.Group("/v1")
 	{
 		posts := v1.Group("/posts"); {
-			posts.GET("getAll/:userId", controllers.Listpost)
-			posts.GET("getOne/:id", controllers.GetOnepost)
-			posts.POST("create", controllers.AddNewpost)
-			posts.PATCH("update/:id", controllers.UpdatePost)
-			posts.DELETE("delete/:id", controllers.Deletepost)
+			posts.GET("getAll/:userId", api.Listpost)
+			posts.GET("getOne/:id", api.GetOnepost)
+			posts.POST("create", api.AddNewpost)
+			posts.PATCH("update/:id", api.UpdatePost)
+			posts.DELETE("delete/:id", api.Deletepost)
 		};
 		comments := v1.Group("/comments"); {
-			comments.GET("getAll/:userId", controllers.GetAllcomment)
-			comments.GET("getOne/:id", controllers.GetOnecomment)
-			comments.POST("create", controllers.AddNewcomment)
-			comments.DELETE("delete/:id", controllers.Deletecomment)
-			comments.PATCH("update/:id", controllers.Updatecomment)
+			comments.GET("getAll/:userId", api.GetAllcomment)
+			comments.GET("getOne/:id", api.GetOnecomment)
+			comments.POST("create", api.AddNewcomment)
+			comments.DELETE("delete/:id", api.Deletecomment)
+			comments.PATCH("update/:id", api.Updatecomment)
 		}
 	}
+
+	return router
 }
