c395889e46304622dbf1d33f99a1b772dbf14d98
Anuj Koli <a.koli@ufl.edu>
2022-02-14 00:15:19 -0500

    Added middleware to router

--- main.go
+++ main.go
@@ -3,19 +3,12 @@ package main
 import (
 	"gatorshare/models"
 	"gatorshare/routes"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
+
 func main() {
-  router := gin.Default()
-
-  router.GET("/", func(responseCtx *gin.Context) {
-    responseCtx.JSON(http.StatusOK, gin.H{"data": "Welcome to Gatorshare made with the help of Go and Gin!"})    
-  })
-
-  routes.InitializeRoutes(router)
-  models.ConnectDatabase()
+  models.Init()
+  DB := models.GetDB()
+  router := routes.InitializeRoutes(DB)
   router.Run(":8080")
 }
