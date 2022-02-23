2ff4ecca42c0e9570ddc0a92b8588f06215172a3
Anuj Koli <a.koli@ufl.edu>
2022-02-23 15:24:22 -0500

    replaced middleware with helper

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"gatorshare/middleware"
+	helper "gatorshare/middleware"
 	"gatorshare/models"
 	"gatorshare/routes"
 )
 
 
 func main() {
-  envsrc := middleware.LoadEnv(".env")
+  envsrc := helper.LoadEnv(".env")
   models.Init(envsrc)
   DB := models.GetDB()
   router := routes.InitializeRoutes(DB)
-  hostport := middleware.GetEnv("PORT", "8080", envsrc)
+  hostport := helper.GetEnv("PORT", "8080", envsrc)
   router.Run(":"+hostport)
 }
