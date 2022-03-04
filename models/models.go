d486a626bfcf2fd10fd4265447dce3b20a57f8a8
Anuj Koli <a.koli@ufl.edu>
2022-03-04 11:17:59 -0500

    Updated schema to have camelCase key in response

--- models/models.go
+++ models/models.go
@@ -6,39 +6,39 @@ import (
 
 type User struct {
 	gorm.Model
-	Username  string `gorm:"uniqueIndex"`
-	Firstname string 
-	Lastname  string 
+	Username  string `json:"userName" gorm:"uniqueIndex"`
+	Firstname string `json:"firstName"`
+	Lastname  string `json:"lastName"`
 	Email     string `gorm:"uniqueIndex"`
-	Zipcode   uint   
-	Avatar    string 
-	Password  string 
-	Bookmarks string 
+	Zipcode   uint   `json:"zipcode"`
+	Avatar    string `json:"avatar"`
+	Password  string `json:"password"`
+	Bookmarks string `json:"bookmark"`
 }
 
 type Post struct {
 	gorm.Model
-	UserID       int 	
+	UserID       int 	 `json:"userId"`
 	User         User
-	Title        string  
-	Description  string  
-	UserLimit    int     
-	Participants string  `gorm:"default:1"`
+	Title        string  `json:"title"`
+	Description  string  `json:"description"`
+	UserLimit    int     `json:"userLimit"`
+	Participants uint  	 `json:"participantNum" gorm:"default:1"`
 	Expiry       float32 `gorm:"default:24"`
-	ViewCount    int64   `gorm:"default:0"`
-	Status       int    
-	Categories   string
-	Tags         string
+	ViewCount    int64   `json:"viewCount" gorm:"default:0"`
+	Status       int     `json:"status"`
+	Categories   string  `json:"categories"`
+	Tags         string  `json:"tags"`
 }
 
 type Comment struct {
 	gorm.Model
-	UserID   uint   
+	UserID   uint    `json:"userId"`
 	User	 User
-	PostID   uint
+	PostID   uint    `json:"postId"`
 	Post	 Post
-	Message  string 
-	ParentId uint   
-	Votes    int    `gorm:"default:0"`
+	Message  string  `json:"message"`
+	ParentId uint    `json:"parentId"`
+	Votes    int     `json:"votes" gorm:"default:0"`
 }
 
