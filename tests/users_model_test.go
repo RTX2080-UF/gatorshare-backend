7131dbd1ed59e9ea08f145b7309eda117140df4a
Ekleen Kaur <eclairss.17@gmail.com>
2022-03-20 05:34:04 +0530

    Added Test GetUserProfile

--- tests/users_model_test.go
+++ tests/users_model_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestRegisterNewUserUnique(t *testing.T) {
+func TestRegisterNewUserUnique(t *testing.T){
 	rnum, _ := rand.Int(rand.Reader, big.NewInt(1000))
 
 	user := &models.User{
@@ -28,4 +28,37 @@ func TestRegisterNewUserUnique(t *testing.T) {
 
   testobj.DB.Delete(user)
   print(res)
+  
 }
+func createUser(t *testing.T)(users *models.User, username string){
+	rnum, _ := rand.Int(rand.Reader, big.NewInt(1000))
+
+	user := &models.User{
+	  Username: "TestUser1" + fmt.Sprint(rnum),
+	  Firstname: "Test User",
+	  Email: "TestUser1" + fmt.Sprint(rnum) + "@gatorshare.com",
+	  Lastname: "1",
+	  Password: "Test",
+  } 
+
+  res, _ := models.AddNewUser(testobj.DB, user)
+  if res == 0 {
+	  t.Error("Unable to create post!")
+	} else {
+	  return user, user.Username
+  	}
+	return
+}
+func TestGetUserProfile(t *testing.T){
+	user, _:= createUser(t)
+	if(user.ID != 0){
+		res := models.GetUserProfile(testobj.DB,user,int(user.ID))
+		if(res == nil){
+			t.Log("Succesfully able to return user")
+		}else {
+			t.Error("Unable to return user!")
+		}
+	}
+}
+
+
