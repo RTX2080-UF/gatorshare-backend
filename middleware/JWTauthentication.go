3f5a3003c629aadc6484f026c6933ba92dd63479
Anuj Koli <a.koli@ufl.edu>
2022-03-20 23:36:11 -0400

    Added jwt token extraction and validation method

--- middleware/JWTauthentication.go
+++ middleware/JWTauthentication.go
@@ -1,11 +1,20 @@
 package middleware
 
 import (
-  "github.com/golang-jwt/jwt"
-  "time"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
-func CreateToken(userid uint, key string) (string, error) {
+var signingKey = GetEnv("ACCESS_SECRET", "Gatorshare", true)
+
+func CreateToken(userid uint) (string, error) {
   var err error
 
   atClaims := jwt.MapClaims{}
@@ -14,10 +23,73 @@ func CreateToken(userid uint, key string) (string, error) {
   atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
   at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 
-  token, err := at.SignedString([]byte(key))
+  token, err := at.SignedString([]byte(signingKey))
   if err != nil {
      return "", err
   }
 
   return token, nil
+}
+
+func TokenValid(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		Pretty(claims)
+	}
+	return nil
+}
+
+func ExtractTokenID(tokenString string) (uint, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint(uid), nil
+	}
+
+	return 0, nil
+}
+
+func ExtractToken(ctx *gin.Context) string {
+	keys := ctx.Request.URL.Query()
+	token := keys.Get("token")
+	if token != "" {
+		return token
+	}
+
+	bearerToken := ctx.Request.Header["Authorization"]
+  if len(strings.Split(bearerToken[0], " ")) == 2 {
+		return strings.Split(bearerToken[0], " ")[1]
+	}
+  return ""
+}
+
+func Pretty(data interface{}) {
+	b, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(b))
 }
