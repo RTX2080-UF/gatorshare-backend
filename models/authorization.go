d9e860b5ea046280dca82a036683e3598f528006
Anuj Koli <a.koli@ufl.edu>
2022-04-11 17:42:42 -0400

    Added upsert to update password and created verify password func

--- models/authorization.go
+++ models/authorization.go
@@ -1,16 +1,31 @@
 package models
 
 import (
+	"gorm.io/gorm/clause"
 	"gorm.io/gorm"
 )
 
 func UpdatePasswordStatus(db *gorm.DB, resetObj ResetPassword) (bool, error) {
 
-	err := db.Create(resetObj).Error
+	err :=db.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "user_id"}},
+		DoUpdates: clause.Assignments(map[string]interface{}{"unique_rnd_str": resetObj.UniqueRndStr}),
+	  }).Create(&resetObj).Error
 	if err != nil {
 		return false, err
 	}
 
 	return true, nil
 
+}
+
+func VerifyPasswordReset(db *gorm.DB, emailId string, token string) (bool, error) {
+	var count int64
+	res := db.Model(&ResetPassword{}).Where("Email = ? AND UniqueRndStr = ? AND Status = ?", emailId, token, true).Count(&count)
+
+	if (count <= 0) {
+		return false, res.Error
+	}
+
+	return true, res.Error
 }
