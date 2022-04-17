c15f0234f11da2cb48ab2557eaf9b32666dd24fe
Anuj Koli <a.koli@ufl.edu>
2022-04-17 16:07:38 -0400

    Created getlatestpost endpoint in controller

--- controllers/home.go
+++ controllers/home.go
@@ -36,3 +36,26 @@ func (base *Controller) GetUserHome(ctx *gin.Context) {
 	
 	middleware.RespondJSON(ctx, http.StatusOK, posts, nil)
 }
+
+func (base *Controller) GetLatestPost(ctx *gin.Context) {
+	uid := middleware.GetUidFromToken(ctx)
+	if uid == 0 {
+		return
+	}
+	var posts []models.Post
+
+	err := models.GetLatestPost(base.DB, ctx, &posts, uid)
+	if err != nil {
+		errCustom := errors.New("unable to retrieve post for user").Error()
+		middleware.RespondJSON(ctx, http.StatusBadGateway, errCustom, err)
+		return
+	}
+ 
+	if (len(posts) == 0) {
+		errCustom := errors.New("no new posts for user")
+		middleware.RespondJSON(ctx, http.StatusNoContent, errCustom.Error(), errCustom)
+		return
+	}
+	
+	middleware.RespondJSON(ctx, http.StatusOK, posts, nil)
+}
