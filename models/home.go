e0aee25189590689c38b7628da2adcd918569369
Anuj Koli <a.koli@ufl.edu>
2022-04-17 16:08:09 -0400

    Created getlatestpost model and pagination function

--- models/home.go
+++ models/home.go
@@ -1,8 +1,32 @@
 package models
 
 import (
+	"net/http"
+	"strconv"
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
+
+func Paginate(r *http.Request) func(db *gorm.DB) *gorm.DB {
+	return func (db *gorm.DB) *gorm.DB {
+	  q := r.URL.Query()
+	  page, _ := strconv.Atoi(q.Get("page"))
+	  if page == 0 {
+		page = 1
+	  }
+  
+	  pageSize, _ := strconv.Atoi(q.Get("page_size"))
+	  switch {
+	  case pageSize > 100:
+		pageSize = 100
+	  case pageSize <= 0:
+		pageSize = 10
+	  }
+  
+	  offset := (page - 1) * pageSize
+	  return db.Offset(offset).Limit(pageSize)
+	}
+}
 
 func GetUserHomePosts(db *gorm.DB, id uint) ([]TagPost, error) {
 	var userLikedTags []TagUser
@@ -16,4 +40,9 @@ func GetUserHomePosts(db *gorm.DB, id uint) ([]TagPost, error) {
 	var tagPosts []TagPost
 	res = db.Preload("Post").Order("created_at desc, updated_at desc").Where("tag_id IN(?)", tagIDs).Find(&tagPosts)
 	return tagPosts, res.Error
+}
+
+func GetLatestPost(db *gorm.DB, ctx *gin.Context, post *[]Post, id uint) (error) {
+	res := db.Scopes(Paginate(ctx.Request)).Order("created_at desc, updated_at desc").Find(&post)
+	return res.Error
 }
