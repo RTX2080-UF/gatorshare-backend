01b819182e185b7ba0719da19cdc6d3abfaefa38
Ekleen Kaur <eclairss.17@gmail.com>
2022-04-20 09:58:43 +0530

    Changes in controllers

--- controllers/users.go
+++ controllers/users.go
@@ -158,7 +158,7 @@ func (base *Controller) DeleteUser(ctx *gin.Context) {
 	uid := middleware.GetUidFromToken(ctx)
 	if uid == 0 {
 		return
-	}A
+	}
 	log.Print("Got request to delete User profile", uid)
 
 	err := models.GetUserProfile(base.DB, &userData, uid)
@@ -309,7 +309,7 @@ func (base *Controller) GetFeedback(ctx *gin.Context) {
 	if uid == 0 {
 		return
 	}
-	err := models.GetUserProfile(base.DB, &feedback, uid)
+	err := models.GetFeedback(base.DB, &feedback, uid)
 
 	if err != nil {
 		errCustom := errors.New("unable to retrieve user feedback with given id").Error()
